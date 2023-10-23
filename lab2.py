23e06dbc637b0f0fca0367827583827d3b81983d
chen bin <CHENB.22@ichat.sp.edu.sg>
2023-10-24 00:16:50 +0800

    update median finding

--- lab2.py
+++ lab2.py
@@ -21,11 +21,19 @@ def find_min_max(tem):
     smallest = min(tem)
     return largest, smallest
 
-def sort_temperature():
-    print("sort_temperature")
+def sort_temperature(tem):
+    tem.sort()
+    return tem
 
-def calc_median_temperature():
-    print("calc_median_temperature")
+def calc_median_temperature(temsorted):
+    length = len(temsorted)
+    if length%2 == 0:
+        middle1 = temsorted[length // 2]
+        middle2 = temsorted[length // 2 - 1]
+        median = (middle1 + middle2) / 2
+    elif length % 2 == 1:
+        median = temsorted[(length+1)//2 -1]
+    return median
 
 display_main_menu()
 z = get_user_input()
@@ -34,4 +42,7 @@ print("average is " + str(average))
 max , min = find_min_max(z)
 print("min is " + str(min) )
 print("max is " + str(max) )
+temsorted = sort_temperature(z)
+median = calc_median_temperature(temsorted)
+print("median number is " + str(median))
 
